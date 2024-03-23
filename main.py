60a0b14e6136ae3c7b7110143dd0be6fdfb5a3a9
sonyakun <owner@sonyakun.com>
2024-03-23 22:44:26 +0900

    .

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ from jinja2 import Environment, FileSystemLoader
 
 env = Environment(loader=FileSystemLoader("./templates"))
 data = {"images": []}
+if os.path.isfile("./_site/index.html"):
+    os.remove("./_site/index.html")
+pc = 0
+while True:
+    if not os.path.exists(f"./_site/{str(pc)}.html"):
+        break
+    if os.path.isfile(f"./_site/{str(pc)}.html"):
+        os.remove(f"./_site/{str(pc)}.html")
+        break
+    pc = pc + 1
 
 
 def generate_tag_notext(images):
@@ -17,8 +27,8 @@ def generate_tag_notext(images):
         <div class="col enter-block">
             <ul class="img_content">
                 <div class="card" style="width: 18rem;">
-                    <a href="{image['url']}" data-lightbox="group"><img src="{image['url']}" width="286px" height="180px"
-                        class="thumbnail_js card-image-top" data-image="{image['url']}"></a>
+                    <a href="{image['url']}" data-lightbox="group"><img src="{image['pre_url']}" width="286px" height="180px"
+                        class="thumbnail_js card-image-top" data-image="{image['pre_url']}"></a>
                     <div class="card-body">
                         <p>撮影日: {image['metadata']['created_at']}</p>
                         <a href="{image['metadata']['world']}" class="btn btn-primary">ワールド</a>
@@ -27,9 +37,12 @@ def generate_tag_notext(images):
                 </div>
             </ul>
         </div>""")
-        if c >= 3:
-            imgs_tag.append('<div class="w-100"></div>')
+        if c >= 7:
+            return "\n".join(imgs_tag)
+        else:
+            c = c + 1
     return "\n".join(imgs_tag)
+
 
 def generate_tag(images):
     c = 0
@@ -39,8 +52,8 @@ def generate_tag(images):
         <div class="col">
             <ul class="img_content">
                 <div class="card" style="width: 18rem;">
-                    <a href="{image['url']}" data-lightbox="group"><img src="{image['url']}" width="286px" height="180px"
-                        class="thumbnail_js card-image-top" data-image="{image['url']}"></a>
+                    <a href="{image['url']}" data-lightbox="group"><img src="{image['pre_url']}" width="286px" height="180px"
+                        class="thumbnail_js card-image-top" data-image="{image['pre_url']}"></a>
                     <div class="card-body">
                         <p class="card-text">{image['text']}</p>
                         <p>撮影日: {image['metadata']['created_at']}</p>
@@ -50,11 +63,14 @@ def generate_tag(images):
                 </div>
             </ul>
         </div>""")
-        if c >= 3:
-            imgs_tag.append('<div class="w-100"></div>')
+        if c >= 7:
+            return "\n".join(imgs_tag)
+        else:
+            c = c + 1
     return "\n".join(imgs_tag)
 
 
+c = 0
 for file in os.listdir("./src"):
     if os.path.isfile(os.path.join("./src", file)):
         if file.endswith(".json"):
@@ -69,11 +85,15 @@ for file in os.listdir("./src"):
                     if sfile["source"]["name"] == "vrc_image_1":
                         d = "2024年3月11日 4時14分"
                 src_url = f"https://github.com/sonyakun-personal/images/blob/master/{sfile["source"]["name"]}.png?raw=true"
+                pre_url = src_url
+                if sfile["_extra"]["low-quality"]:
+                    pre_url = sfile["_extra"]["low-quality"]
                 if sfile["text"] is None:
                     template = env.get_template("index_notext.html")
                     data["images"].append(
                         {
                             "url": src_url,
+                            "pre_url": pre_url,
                             "metadata": {
                                 "created_at": d,
                                 "world": sfile["metadata"]["world"],
@@ -82,15 +102,14 @@ for file in os.listdir("./src"):
                             "generate_tag_notext": generate_tag_notext,
                         }
                     )
-                    rendered = template.render(
-                        data, generate_tag_notext=generate_tag_notext
-                    )
+                    gt = generate_tag_notext
 
                 else:
                     template = env.get_template("index.html")
                     data["images"].append(
                         {
                             "url": src_url,
+                            "pre_url": pre_url,
                             "text": sfile["text"].replace("\n", "<br>"),
                             "metadata": {
                                 "created_at": d,
@@ -100,7 +119,35 @@ for file in os.listdir("./src"):
                             "generate_tag": generate_tag,
                         }
                     )
-                    rendered = template.render(data, generate_tag=generate_tag)
+                    gt = generate_tag
+    if c >= 7:
+        rendered = template.render(data, generate_tag=gt)
+        if os.path.isfile("./_site/index.html"):
+            pc = 0
+            while True:
+                if not os.path.isfile(f"./_site/{str(pc)}.html"):
+                    with open(f"./_site/{str(pc)}.html", "w", encoding="utf-8") as f:
+                        f.write(str(rendered))
+                        break
+                pc = pc + 1
+        else:
+            with open("./_site/index.html", "w", encoding="utf-8") as f:
+                f.write(str(rendered))
+        data["page"] = []
+        data["images"] = []
+        c = 0
+    else:
+        c = c + 1
 
-with open("./_site/index.html", "w", encoding="utf-8") as f:
-    f.write(str(rendered))
+rendered = template.render(data, generate_tag=gt)
+if os.path.isfile("./_site/index.html"):
+    pc = 0
+    while True:
+        if not os.path.isfile(f"./_site/{str(pc)}.html"):
+            with open(f"./_site/{str(pc)}.html", "w", encoding="utf-8") as f:
+                f.write(str(rendered))
+                break
+        pc = pc + 1
+else:
+    with open("./_site/index.html", "w", encoding="utf-8") as f:
+        f.write(str(rendered))
